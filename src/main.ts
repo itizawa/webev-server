541251f28d738d0c1fd0502de6fce14d6bf7a54c
itizawa <itizawa.pen@gmail.com>
2022-04-16 19:52:28 +0900

    ✨ sync

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/*****************************
+ * Main Process              *
+ *****************************/
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = Number(process.env.PORT) || 8000; // Cloud Run の要件。環境変数PORTで起動するように。
+  await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
 }
 bootstrap();
