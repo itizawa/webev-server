987de1c49458e693e789e398e50a212fb749cddb
itizawa <itizawa.pen@gmail.com>
2022-04-17 12:18:01 +0900

    📝 enable docs

--- src/presentation/dto/ogp/fetchOgp.dto.ts
+++ src/presentation/dto/ogp/fetchOgp.dto.ts
@@ -1,11 +1,11 @@
-// import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsUrl } from 'class-validator';
 
 export class FetchOgpDto {
-  //   @ApiProperty({
-  //     description: '取得する対象のurl',
-  //     default: 'https://onn-hr.com/',
-  //   })
+  @ApiProperty({
+    description: '取得する対象のurl',
+    default: 'https://onn-hr.com/',
+  })
   @IsUrl({}, { message: 'urlは正しい形式である必要があります' })
   url: string;
 }
