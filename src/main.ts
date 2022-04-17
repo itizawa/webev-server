43887eff4f017da51e836bed8e4b8aa993791471
itizawa <itizawa.pen@gmail.com>
2022-04-17 11:52:22 +0900

    👷 setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from '~/app.module';
 
 /*****************************
  * Main Process              *
  *****************************/
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Webev Server')
+    .setDescription('Webevのバックエンドサーバーです')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = Number(process.env.PORT) || 8000; // Cloud Run の要件。環境変数PORTで起動するように。
   await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
 }
