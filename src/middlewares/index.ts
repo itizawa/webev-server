e7d4169971b435eaaf52022617a3c352a68188a1
itizawa <itizawa.pen@gmail.com>
2022-05-15 20:02:05 +0900

    🔥 delete

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,3 @@
-export { accessTokenParser } from './access-token-parser';
 export { loginRequired } from './login-required';
 export { requestLoggerMiddleware } from './request-logger';
 export { validate } from './validate';
