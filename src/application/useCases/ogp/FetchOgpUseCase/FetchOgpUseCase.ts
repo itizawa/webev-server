ac76cdcab2a43178bcbafeff9a1c042a8764b86f
itizawa <itizawa.pen@gmail.com>
2022-05-17 01:47:38 +0900

    ✨ rename method

--- src/application/useCases/ogp/FetchOgpUseCase/FetchOgpUseCase.ts
+++ src/application/useCases/ogp/FetchOgpUseCase/FetchOgpUseCase.ts
@@ -6,7 +6,7 @@ export class FetchOgpUseCase {
    * Ogp を取得する
    * @param url {string} 対象のurl
    */
-  async fetchOgp(url: string): Promise<{ [key: string]: string }> {
+  async execute(url: string): Promise<{ [key: string]: string }> {
     return await this.ogpAdapter.fetch(url);
   }
 }
