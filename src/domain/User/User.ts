fb5b1d4f8e69ccb457e89034a7deed3f601c79cd
itizawa <itizawa.pen@gmail.com>
2022-05-21 20:41:49 +0900

    ✨ update domain

--- src/domain/User/User.ts
+++ src/domain/User/User.ts
@@ -1,19 +1,24 @@
 export class User {
   id: string;
   username: string;
-  password: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
   constructor(init: User) {
     this.id = init.id;
     this.username = init.username;
-    this.username = init.username;
+    this.email = init.email;
+    this.createdAt = init.createdAt;
+    this.updatedAt = init.updatedAt;
   }
 
   public static create(params: Optional<User, 'id'>) {
     return new User({
-      // TODO
       id: params.id || '1',
       username: params.username,
-      password: params.password,
+      email: params.email,
+      createdAt: new Date(),
+      updatedAt: new Date(),
     });
   }
 }
