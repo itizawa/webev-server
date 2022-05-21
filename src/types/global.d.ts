d3324979c19e5d088c72605e8f3daed0983e432b
itizawa <itizawa.pen@gmail.com>
2022-05-21 21:16:15 +0900

    🔥 delete

--- src/types/global.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 一部のkeyをoptionalにします
- * @example
- * type User = { name: string, email: string, age: number }
- * Optional<User, "name" | "age">
- * // => { name?: string, email: string, age?: number }
- */
-declare type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
