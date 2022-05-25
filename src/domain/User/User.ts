8989e22b71eb0985bad4eb0e864b07c025adb73c
itizawa <itizawa.pen@gmail.com>
2022-05-25 23:53:51 +0900

    🏷️ define user

--- src/domain/User/User.ts
+++ src/domain/User/User.ts
@@ -1,5 +1,22 @@
 import { ObjectId } from 'mongodb';
 
+/**
+ * @swagger
+ * definitions:
+ *   User:
+ *     type: object
+ *     properties:
+ *       id:
+ *         type: string
+ *       username:
+ *         type: string
+ *       email:
+ *         type: string
+ *       createdAt:
+ *         type: string
+ *       updatedAt:
+ *         type: string
+ */
 export class User {
   id: string;
   username: string;
