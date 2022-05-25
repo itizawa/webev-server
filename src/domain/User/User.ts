125cf6945c4acad88a1ea5a4ed371aaee08a86cc
itizawa <itizawa.pen@gmail.com>
2022-05-26 00:41:50 +0900

    🏷️ update user

--- src/domain/User/User.ts
+++ src/domain/User/User.ts
@@ -12,6 +12,8 @@ import { ObjectId } from 'mongodb';
  *         type: string
  *       email:
  *         type: string
+ *       profileUrl:
+ *         type: string
  *       createdAt:
  *         type: string
  *       updatedAt:
@@ -21,12 +23,14 @@ export class User {
   id: string;
   username: string;
   email: string;
+  profileUrl: string;
   createdAt: Date;
   updatedAt: Date;
   constructor(init: User) {
     this.id = init.id;
     this.username = init.username;
     this.email = init.email;
+    this.profileUrl = init.profileUrl;
     this.createdAt = init.createdAt;
     this.updatedAt = init.updatedAt;
   }
@@ -37,6 +41,7 @@ export class User {
       id: new ObjectId().toString(),
       username: params.username,
       email: params.email,
+      profileUrl: params.profileUrl,
       createdAt: new Date(),
       updatedAt: new Date(),
     });
