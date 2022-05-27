2d5e666f18b7f3a5aac519598884eaf3e67e1df8
itizawa <itizawa.pen@gmail.com>
2022-05-28 01:42:00 +0900

    ✨ implement route

--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -1,11 +1,15 @@
 import * as express from 'express';
 import { logger } from '~/utils/logger';
+import { loginRequired } from '../middlewares';
 
 import { getOgps } from './ogp';
+import { postPageByUrl } from './page';
 import { getCurrentUser } from './user';
 
 export const setupExpressRoutes = (express: express.Express): void => {
   express.get('/api/v1/ogps', getOgps);
+
+  express.post('/api/v1/pages', loginRequired, postPageByUrl);
 
   express.get('/api/v1/users/me', getCurrentUser);
 
