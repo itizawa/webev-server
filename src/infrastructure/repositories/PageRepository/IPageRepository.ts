743d0f698e836ef2d21367a60b1e4eba6a837338
itizawa <itizawa.pen@gmail.com>
2022-05-28 11:10:38 +0900

    ✨ implement PageRepository

--- src/infrastructure/repositories/PageRepository/IPageRepository.ts
+++ src/infrastructure/repositories/PageRepository/IPageRepository.ts
@@ -1,8 +1,10 @@
 import { Page } from '~/domain/Page';
+import { PaginationResult } from '~/domain/shared';
 
 export interface IPageRepository {
   create(pages: Page): Promise<Page>;
   count(): Promise<number>;
+  findPagesByUserId(userId: string): Promise<PaginationResult<Page>>;
   // find(query: FilterQuery<Page>, options: PaginationOptions): Promise<PaginationResult<Page>>;
   findById(id: string): Promise<Page | null>;
   update(id: string, newObject: Partial<Page>): Promise<Page | null>;
