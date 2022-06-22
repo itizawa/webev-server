7f610c9864667facbb7417b165bed9a43b992dd4
itizawa <itizawa.pen@gmail.com>
2022-06-23 01:01:05 +0900

    🐛 fix

--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -25,11 +25,11 @@ export const setupExpressRoutes = (express: express.Express): void => {
 
   // pages
   express.post('/api/v1/pages', loginRequired, postPageByUrl);
-  express.get('/api/v1/pages/:id', loginRequired, findPage);
   express.delete('/api/v1/pages/:id', loginRequired, deletePageById);
-  express.put('/api/v1/pages/:pageId', loginRequired, updatePage);
   express.get('/api/v1/pages/all-count', countAllPages);
   express.get('/api/v1/pages/list', loginRequired, findPages);
+  express.put('/api/v1/pages/:pageId', loginRequired, updatePage);
+  express.get('/api/v1/pages/:id', loginRequired, findPage);
 
   // users
   express.get('/api/v1/users/me', getCurrentUser);
