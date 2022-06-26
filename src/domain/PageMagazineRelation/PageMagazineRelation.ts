1ff48acda5d3da3b8191e0986493aad4f7836c13
itizawa <itizawa.pen@gmail.com>
2022-06-26 22:17:23 +0900

    🏷️ update type

--- src/domain/PageMagazineRelation/PageMagazineRelation.ts
+++ src/domain/PageMagazineRelation/PageMagazineRelation.ts
@@ -2,12 +2,14 @@ export class PageMagazineRelation {
   id: string;
   pageId: string;
   magazineId: string;
+  createdUserId: string;
   createdAt: Date;
   updatedAt: Date;
   constructor(init: PageMagazineRelation) {
     this.id = init.id;
     this.pageId = init.pageId;
     this.magazineId = init.magazineId;
+    this.createdUserId = init.createdUserId;
     this.createdAt = init.createdAt;
     this.updatedAt = init.updatedAt;
   }
