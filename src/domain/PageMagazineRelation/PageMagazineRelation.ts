19ee304d373bbb2e9d054f922dd24b879e94fb88
itizawa <itizawa.pen@gmail.com>
2022-06-26 22:44:05 +0900

    🏷️ update

--- src/domain/PageMagazineRelation/PageMagazineRelation.ts
+++ src/domain/PageMagazineRelation/PageMagazineRelation.ts
@@ -1,3 +1,5 @@
+import { ObjectId } from 'mongodb';
+
 export class PageMagazineRelation {
   id: string;
   pageId: string;
@@ -12,5 +14,19 @@ export class PageMagazineRelation {
     this.createdUserId = init.createdUserId;
     this.createdAt = init.createdAt;
     this.updatedAt = init.updatedAt;
+  }
+
+  public static create(
+    params: Omit<PageMagazineRelation, 'id' | 'createdAt' | 'updatedAt'>,
+  ) {
+    return new PageMagazineRelation({
+      // 本来生成する場所はdomainに書くべきではない
+      id: new ObjectId().toString(),
+      pageId: params.pageId,
+      createdUserId: params.createdUserId,
+      magazineId: params.magazineId,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
   }
 }
