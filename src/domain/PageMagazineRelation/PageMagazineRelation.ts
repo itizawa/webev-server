239d354c95c3bb697a8795f5f519af29157ee280
itizawa <itizawa.pen@gmail.com>
2022-06-26 22:08:03 +0900

    ✨ implement PageMagazineRelation

---
+++ src/domain/PageMagazineRelation/PageMagazineRelation.ts
@@ -0,0 +1,14 @@
+export class PageMagazineRelation {
+  id: string;
+  pageId: string;
+  magazineId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  constructor(init: PageMagazineRelation) {
+    this.id = init.id;
+    this.pageId = init.pageId;
+    this.magazineId = init.magazineId;
+    this.createdAt = init.createdAt;
+    this.updatedAt = init.updatedAt;
+  }
+}
